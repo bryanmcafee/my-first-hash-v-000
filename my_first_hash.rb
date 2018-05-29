683de3dd58e873487e247cff172ce02ad82459cd
Bryan McAfee <brymcafee@gmail.com>
2018-05-29 17:28:30 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -32,7 +32,6 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
 shipping_manifest ["muskets"] = 2
-end
 
   # add 4 gun powder to the shipping_manifest hash below
 shipping_manifest ["gun powder"] = 4
@@ -40,5 +39,4 @@ shipping_manifest ["gun powder"] = 4
 
   # return the shipping_manifest hash below
 
-end
 end
